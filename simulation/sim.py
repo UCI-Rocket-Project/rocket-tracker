e0d117d36fe03041c59a51cff191a03193c89dc5
Eric Pedley <ericpedley@gmail.com>
2023-10-22 11:27:05 -0700

    get ground truth pixel coordinates

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,6 +1,7 @@
-from math import pi, sin, cos
 import numpy as np
+import os
 from time import sleep
+import cv2 as cv
 
 
 from direct.showbase.ShowBase import ShowBase
@@ -54,18 +55,20 @@ class Sim(ShowBase):
 
         self.taskMgr.add(self.rocketPhysicsTask, "Physics")
 
-        # Load and transform the panda actor.
+        self.camera_dist = 600
 
         self.rocket_model = self.loader.loadModel("models/panda-model")
-        self.rocket_model.setScale(0.005, 0.005, 0.005) # the panda is about 5 meters long
+        self.rocket_model.setScale(0.005, 0.005, 0.005) # the panda is about 5 meters long after this scaling
         self.rocket_model.setHpr(0,-90,90)
+        self.rocket_model.setPos(0,self.camera_dist,0)
         self.rocket_model.reparentTo(self.render)
 
         self.rocket = Rocket()
-        self.camera_dist = 600
-        self.camera.setPos(0,-self.camera_dist,0) # https://docs.panda3d.org/1.10/python/reference/panda3d.core.Camera#panda3d.core.Camera
+        self.camera.setPos(0,0,0) # https://docs.panda3d.org/1.10/python/reference/panda3d.core.Camera#panda3d.core.Camera
         # self.camLens.setFov(0.9)
-        self.camLens.setFov(0.9)
+        self.camera_fov = 0.9
+        self.camLens.setFov(self.camera_fov)
+        self.camera_res = (958, 1078)
         T.AbortSlew()
         T.SlewToAltAzAsync(0,0)
         while T.Slewing:
@@ -77,10 +80,46 @@ class Sim(ShowBase):
 
     def rocketPhysicsTask(self, task):
         self.rocket.step(task.time)
-        self.rocket_model.setPos(0,0,self.rocket.height)
+        self.rocket_model.setPos(0,self.camera_dist,self.rocket.height)
         return Task.cont
 
-    # Define a procedure to move the camera.
+    def getImage(self):
+        self.screenshot()
+        files_list = os.listdir(".")
+        img = None
+        for f in files_list:
+            if f.endswith(".jpg"):
+                img = cv.imread(f)
+                os.remove(f)
+                break
+        return img
+    
+    def getGroundTruthRocketPixelCoordinates(self):
+        alt, az = np.deg2rad(T.Altitude), np.deg2rad(T.Azimuth)
+        alt_rotation = np.array([
+            [1,  0,  0],
+            [0,  np.cos(alt),  -np.sin(alt)],
+            [0,  np.sin(alt), np.cos(alt)] 
+        ])
+
+        az_rotation = np.array([
+            [np.cos(az),  -np.sin(az),  0],
+            [np.sin(az),  np.cos(az),  0],
+            [0,  0,  1]
+        ])
+
+        rocket_pos = np.array([0,self.camera_dist,self.rocket.height])
+
+        rocket_cam_pos = alt_rotation.T @ az_rotation.T @ rocket_pos
+
+        w,h = self.camera_res
+
+        focal_len_pixels = w/(2*np.tan(np.deg2rad(self.camera_fov/2)))
+
+        pixel_x = w/2 + focal_len_pixels * rocket_cam_pos[0]/rocket_cam_pos[1] 
+        pixel_y = h/2 - focal_len_pixels * rocket_cam_pos[2]/rocket_cam_pos[1] 
+
+        return int(pixel_x), int(pixel_y)
 
     def spinCameraTask(self, task):
 
@@ -95,12 +134,16 @@ class Sim(ShowBase):
         # T.SlewToAltAzAsync(0,angleDegrees)
         control_input = KP*err + KI*self.total_err - KD * d
         clipped_input = np.clip(control_input,-6,6)
-        print(control_input, setpoint, curr, clipped_input)
+        # print(control_input, setpoint, curr, clipped_input)
         T.MoveAxis(TelescopeAxes.axisSecondary, -clipped_input)
         self.camera.setHpr(T.Azimuth,T.Altitude,0)
-
-        # self.screenshot()
-
+        img = self.getImage()
+        if img is None:
+            return Task.cont
+        x,y = self.getGroundTruthRocketPixelCoordinates()
+        print(x,y)
+        cv.circle(img, [x,y], 10, (255,0,0), -1)
+        cv.imwrite("latest.png", img) 
         return Task.cont
 
 
