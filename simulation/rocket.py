7632a519b544da05810bdc9fc7964284c0b6db82
Eric Pedley <ericpedley@gmail.com>
2023-10-23 20:55:27 -0700

    make trajectory make more sense

--- simulation/rocket.py
+++ simulation/rocket.py
@@ -1,17 +1,10 @@
 from read_telem import read_telemetry, get_positions
 import numpy as np
 
-def eval_cubic(coefficients, t):
-    a,b,c,d = coefficients
-    return a*t**3 + b*t**2 + c*t + d
+def eval_poly(coefficients, t):
+    n = len(coefficients)
+    return sum([coefficients[i]*t**(n-i-1) for i in range(n)])
 
-def eval_quadratic(coefficients, t):
-    a,b,c = coefficients
-    return a*t**2+b*t+c
-
-def eval_linear(coefficients, t):
-    a,b = coefficients
-    return a*t+b
 class Rocket:
     def __init__(self):
         self.engine_cutoff_time = 23
@@ -21,7 +14,8 @@ class Rocket:
         pre_cutoff = positions[positions[:,0]<self.engine_cutoff_time]
         self.x_coeff_1 = np.polyfit(pre_cutoff[:,0], pre_cutoff[:,1],2)
         self.y_coeff_1 = np.polyfit(pre_cutoff[:,0], pre_cutoff[:,2],2)
-        self.z_coeff_1 = np.polyfit(pre_cutoff[:,0], pre_cutoff[:,3],3)
+        self.z_coeff_1 = np.polyfit(pre_cutoff[:,0], pre_cutoff[:,3],5)
+        self.z_coeff_1[-1]=0 # start at h=0
         post_cutoff = positions[positions[:,0]>=self.engine_cutoff_time]
         self.x_coeff_2 = np.polyfit(post_cutoff[:,0], post_cutoff[:,1],1)
         self.y_coeff_2 = np.polyfit(post_cutoff[:,0], post_cutoff[:,2],1)
@@ -38,11 +32,11 @@ class Rocket:
             self.position = [
                 0,#eval_quadratic(self.x_coeff_1, time),
                 0,#eval_quadratic(self.y_coeff_1, time),
-                eval_cubic(self.z_coeff_1, time)
+                eval_poly(self.z_coeff_1, time)
             ]
         else:
             self.position = [
                 0,#eval_linear(self.x_coeff_2,time),
                 0,#eval_linear(self.y_coeff_2,time),
-                eval_linear(self.z_coeff_2,time)
+                eval_poly(self.z_coeff_2,time)
             ]
