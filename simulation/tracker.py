a5e7c550c9e44d752a2f2e1fad0c03b535a46468
Eric Pedley <ericpedley@gmail.com>
2023-11-16 13:12:45 -0800

    add image depth info to kalman filter

--- simulation/tracker.py
+++ simulation/tracker.py
@@ -5,7 +5,13 @@ from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import cv2 as cv
 from filterpy.kalman import UnscentedKalmanFilter, JulierSigmaPoints
-from utils import GroundTruthTrackingData, TelemetryData, gps_to_enu, enu_to_gps
+from utils import GroundTruthTrackingData, TelemetryData, enu_to_gps
+from dataclasses import dataclass
+
+@dataclass
+class Feature:
+    descriptor: np.ndarray
+    size: float
 
 class Tracker:
     def __init__(self, 
@@ -23,6 +29,7 @@ class Tracker:
         `rocket_initial_position`: initial position of the rocket in GPS coordinates (lat,lng)
         `mount_initial_position`: initial position of the mount in GPS coordinates (lat,lng)
         '''
+
         self.camera_res = camera_res
         self.focal_len = focal_len
         self.logger = logger
@@ -34,13 +41,14 @@ class Tracker:
 
         self.filter = UnscentedKalmanFilter(
             dim_x = state_dimensionality, # state dimension (xyz plus their 1st and 2nd derivatives)
-            dim_z = 5, # observation dimension (altitude, azimuth, lat,lng, altimeter reading),
+            dim_z = 6, # observation dimension (altitude, azimuth, scale, lat,lng, altimeter reading),
             dt = 1/30,
             hx = self._measurement_function,
             fx = self._rocket_state_transition,
             points = JulierSigmaPoints(state_dimensionality)
         )
 
+        self.rocket_initial_position = rocket_initial_position
         self.filter.x  = np.zeros(state_dimensionality)
         self.filter.x[0], self.filter.x[3], self.filter.x[6] = rocket_initial_position
         self.previous_filter_predict_time = 0
@@ -48,6 +56,7 @@ class Tracker:
         self.telescope = telescope
         self.feature_detector = cv.SIFT_create()
         self.target_feature: np.ndarray = None # feature description 
+        self.initial_feature_size = None
         self.SCALE_FACTOR = 4
 
     def _rocket_state_transition(self, state: np.ndarray, dt):
@@ -75,16 +84,21 @@ class Tracker:
         x,y,z = state[0],state[3],state[6]
         alt = np.rad2deg(np.arctan2(z, np.sqrt(x**2 + y**2)))
         az = -np.rad2deg(np.arctan2(x, y))
-        lat,lng, height = enu_to_gps(np.array([x,y,z]), self.gps_pos)
-        return np.array([alt,az,lat,lng,height])
+        lat, lng, height = enu_to_gps(np.array([x,y,z]), self.gps_pos)
+        initial_dist = np.linalg.norm(self.rocket_initial_position)
+        new_dist = np.linalg.norm([x,y,z])
+        scale = new_dist/initial_dist
+        
+        return np.array([alt,az,scale,lat,lng,height])
 
-    def estimate_az_alt_from_img(self, img: np.ndarray, global_step: int, gt_pos: tuple[int,int]) -> tuple[float,float]:
+    def estimate_az_alt_scale_from_img(self, img: np.ndarray, global_step: int, gt_pos: tuple[int,int]) -> tuple[float,float,float]:
         '''
         `img`: image from camera
         `global_step`: current time step (for logging)
         `gt_pos`: ground truth pixel position of rocket (for logging error and mocking out the pixel tracking algorithm)
 
-        returns (alt,az)
+        returns (alt,az,scale) where alt and az are in degrees and scale has no units, it's just the current apparent object size
+        divided by the initial apparent object size
         '''
         gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
         gray = cv.resize(gray, np.array(img.shape)[:2]//self.SCALE_FACTOR) # resize to make computation faster
@@ -92,45 +106,42 @@ class Tracker:
         points = np.array([kp.pt for kp in keypoints])
 
         if len(keypoints) == 0:
-            return None, None
-        else:
-            # show image with keypoints
-            vis = cv.drawKeypoints(gray,keypoints,None,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            cv.imwrite("features.png",vis)
+            return None, None, None
 
         center = np.array(gray.shape)//2
         if self.target_feature is None:
             closest_dist = np.linalg.norm(center*2)
-            for dist, description in zip(np.linalg.norm(points-center,axis=1),descriptions):
+            for dist, description, keypoint in zip(np.linalg.norm(points-center,axis=1),descriptions, keypoints):
                 if dist<closest_dist:
-                    self.target_feature = description
+                    self.target_feature = Feature(description, keypoint.size)
+                    self.initial_feature_size = keypoint.size
                     closest_dist = dist
         
         # find coordinates of closest feature point
         max_similarity = 0
         new_pos = None
         new_feature = None
-        similarities = descriptions @ self.target_feature
+        similarities = descriptions @ self.target_feature.descriptor
         for i, (keypoint, similarity) in enumerate(zip(keypoints,similarities)):
             if similarity > max_similarity:
                 max_similarity = similarity
                 new_pos = np.array(keypoint.pt).astype(int)
-                new_feature = descriptions[i]
+                new_feature = Feature(descriptions[i], keypoint.size)
+        self.target_feature = new_feature
 
         if new_pos is None:
-            return None, None
+            return None, None, None
 
         altitude_from_image_processing = self.telescope.Altitude+np.rad2deg(np.arctan((center[1]-new_pos[1])*self.SCALE_FACTOR/self.focal_len))
         azimuth_from_image_processing = self.telescope.Azimuth+np.rad2deg(np.arctan((center[0]-new_pos[0])*self.SCALE_FACTOR/self.focal_len))
         
-        self.target_feature = new_feature
         self.logger.add_scalar("Pixel Estimate Error (X)",new_pos[0]*self.SCALE_FACTOR-gt_pos[0],global_step)
         self.logger.add_scalar("Pixel Estimate Error (Y)",new_pos[1]*self.SCALE_FACTOR-gt_pos[1],global_step)
 
         self.logger.add_scalar("Imaging Altitude Estimate (Degrees)",altitude_from_image_processing,global_step)
         self.logger.add_scalar("Imaging Azimuth Estimate (Degrees)",azimuth_from_image_processing,global_step)
 
-        return altitude_from_image_processing, azimuth_from_image_processing
+        return altitude_from_image_processing, azimuth_from_image_processing, new_feature.size/self.initial_feature_size
 
 
     def update_tracking(self, img: np.ndarray, telem_measurements: TelemetryData, global_step: int, ground_truth: GroundTruthTrackingData) -> tuple[int,int]:
@@ -142,7 +153,7 @@ class Tracker:
         is at (0,0,0) and (0,0) az/alt is  towards positive Y, and Z is up
         '''
 
-        altitude_from_image_processing, azimuth_from_image_processing = self.estimate_az_alt_from_img(img, global_step, ground_truth.pixel_coordinates) or (None,None)
+        altitude_from_image_processing, azimuth_from_image_processing, img_scale = self.estimate_az_alt_scale_from_img(img, global_step, ground_truth.pixel_coordinates)
 
         using_image_processing = altitude_from_image_processing is not None
 
@@ -153,13 +164,18 @@ class Tracker:
         self.previous_filter_predict_time = global_step
 
         # TODO: set measurement noise really high for any missing measurements
+        np.set_printoptions(suppress=True, precision=5)
         if using_image_processing:
             measurement_vector = np.array([altitude_from_image_processing,
                 azimuth_from_image_processing, 
+                img_scale,
                 telem_measurements.gps_lat,
                 telem_measurements.gps_lng,
                 telem_measurements.altimeter_reading
             ])
+            # print(measurement_vector)
+            # print(self._measurement_function(self.filter.x))
+            # print()
             self.filter.update(measurement_vector)
                             
         self.logger.add_scalar("Kalman Filter x", self.filter.x[0], global_step)
