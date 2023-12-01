38bf08871554dcf35f02a055013a61a99cba8ae0
Eric Pedley <ericpedley@gmail.com>
2023-12-01 00:15:25 -0800

    make camera code modular

--- camera.py
+++ camera.py
@@ -1,28 +1,20 @@
 import camera_zwo_asi
 import cv2 as cv
-import numpy as np 
+import numpy as np
 
-# connecting to the camera
-# at index 0
-camera = camera_zwo_asi.Camera(0)
+class Camera:
+    def __init__(self):
+        self.camera = camera_zwo_asi.Camera(0)
+        self.camera.set_control('Gain', 300)
+        self.camera.set_control('Exposure', 'auto')
 
-# printing information in the
-# terminal
+    def take_picture(self) -> cv.Mat:
+        img: np.ndarray = self.camera.capture().get_image()
+        img = cv.cvtColor(img, cv.COLOR_BAYER_RGGB2BGR)
+        return img
 
-# changing some controllables
-# (supported arguments: the one that are
-# indicated as 'writable' in the information
-# printed above)
-camera.set_control("Gain",300)
-camera.set_control("Exposure","auto")
-
-# taking the picture
-# filepath and show are optional, if you do not
-# want to save the image or display it
-img: np.ndarray = camera.capture().get_image()
-img = cv.cvtColor(img, cv.COLOR_BAYER_RGGB2BGR)
-print(img.shape)
-cv.imshow("img",img)
-cv.waitKey(0)
-
-# getting a flat numpy array with the image data
+if __name__ == '__main__':
+    cam = Camera()
+    img = cam.take_picture()
+    cv.imshow("Camera Image", img)
+    cv.waitKey(0)
