8874e99f65eb7182bba25d7750933a2b8afac5e4
Eric Pedley <ericpedley@gmail.com>
2023-12-01 00:29:35 -0800

    add class to encapsulate telescope functionality

---
+++ telescope.py
@@ -0,0 +1,21 @@
+from alpaca.telescope import TelescopeAxes, Telescope as AlpacaTelescope
+
+class Telescope:
+    def __init__(self, azimuth=0, altitude=0):
+        self.T = AlpacaTelescope('127.0.0.11111', 0)
+        self.T.EquatorialSystem
+    
+    def slewAzimuthRate(self, rate):
+        self.T.MoveAxis(TelescopeAxes.axisPrimary, rate)
+
+    def slewAltitudeRate(self, rate, time):
+        self.T.MoveAxis(TelescopeAxes.axisSecondary, rate)
+    
+    @property
+    def Azimuth(self):
+        return self.T.Azimuth
+
+    @property
+    def Altitude(self):
+        return self.T.Altitude
+
