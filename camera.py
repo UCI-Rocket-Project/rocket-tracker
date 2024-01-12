8496fef417cc0671ef21fe1b808c5130a9823396
Eric Pedley <ericpedley@gmail.com>
2024-01-12 12:18:51 -0800

    got manual working fine

--- camera.py
+++ camera.py
@@ -3,15 +3,23 @@ import cv2 as cv
 import numpy as np
 
 class Camera:
-    def __init__(self):
+    def __init__(self, gain=300, exposure='auto'):
         self.camera = camera_zwo_asi.Camera(0)
-        self.camera.set_control('Gain', 300)
-        self.camera.set_control('Exposure', 'auto')
+        self.camera.set_control('Gain', gain)
+        self.camera.set_control('Exposure', exposure)
 
     def take_picture(self) -> cv.Mat:
         img: np.ndarray = self.camera.capture().get_image()
         img = cv.cvtColor(img, cv.COLOR_BAYER_RGGB2BGR)
         return img
+    
+    def set_gain(self, new_gain: int):
+        self.camera.set_control('Gain', new_gain)
+
+    def set_exposure(self, new_exposure: int):
+        self.camera.set_control('Exposure', new_exposure)
+
+
 
 if __name__ == '__main__':
     cam = Camera()
--- control_telescope.py
+++ control_telescope.py
@@ -1,17 +1,18 @@
 import pygame
 from telescope import Telescope
 from camera import Camera
-# from simulation.tracker import Tracker
+from simulation.tracker import Tracker
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import cv2 as cv
-from time import time
+from time import time, strftime
 
 t = Telescope()
-cam = Camera()
+gain = 300
+cam = Camera(gain=gain)
 focal_length_pixels = 12.5 / 2.9e-3
 logger = SummaryWriter(f'runs/telescope_control')
-# tracker = Tracker((1920, 1080), focal_length_pixels, logger, t, (0,0,0), (0,0,-100))
+tracker = Tracker((1920, 1080), focal_length_pixels, logger, t, (0,0,0), (0,0,-100))
 
 pygame.init()
 pygame.joystick.init()
@@ -25,6 +26,17 @@ LEFT_AXIS_X = 0
 LEFT_AXIS_Y = 1
 RIGHT_AXIS_X = 3
 RIGHT_AXIS_Y = 4
+
+BUTTON_X = 0
+BUTTON_CIRCLE = 1
+BUTTON_TRIANGLE = 2
+BUTTON_SQUARE = 3
+
+BUTTON_LEFT_BUMPER = 4
+BUTTON_RIGHT_BUMPER = 5
+
+BUTTON_LEFT_TRIGGER = 6
+BUTTON_RIGHT_TRIGGER = 7
 
 def joystick_control():
     
@@ -50,35 +62,98 @@ def joystick_control():
 
     t.slew_rate_azi_alt(slew_x, slew_y)
 
-# def tracker_control(img):
-#     tracker.update_tracking(img, None, 0, None)
+recording = False
+video = None
+tracking = False
+
+def handle_button_press(button: int):
+    global recording, video, gain, tracking
+    if button == BUTTON_CIRCLE:
+        if not recording:
+            # start recording
+            video = cv.VideoWriter(f"video_{strftime('%m_%m_%d-%H:%M')}.avi", cv.VideoWriter_fourcc(*'MJPG'), 10, (1920,1080))
+        else:
+            # stop recording
+            video.release()
+            video = None
+        recording = not recording
+    
+    elif button == BUTTON_RIGHT_BUMPER:
+        gain += 100
+        cam.set_gain(gain)
+    
+    elif  button == BUTTON_RIGHT_TRIGGER:
+        gain -= 50 
+        cam.set_gain(gain)
+
+    elif button == BUTTON_TRIANGLE:
+        print("Tracking toggled")
+        tracking = not tracking
+
+
+def tracker_control(img):
+    tracker.update_tracking(img, None, 0, None)
 
 start_time_tenth_second = (time()%1) 
-# main loop
-while True:
+
+def main():
     # retrieve any events ...
     for event in pygame.event.get():
         if event.type == pygame.JOYAXISMOTION:
             axis[event.axis] = event.value
-    joystick_control()
+        if event.type == pygame.JOYBUTTONDOWN:
+            handle_button_press(event.button) 
+
 
     img = cam.take_picture()
+    if tracking and False:
+        tracker_control(img)
+    else:
+        joystick_control()
 
-    # draw a crosshair
+    # # draw current position
+    cv.rectangle(img, (10,50), (200,0), (0,0,0), -1)
+    # azi, alt = t.read_position()
+    readout_text = ""
+    readout_text += f"Gain: {gain}"
+    # readout_text += f"\nazi: {azi:.2f} alt: {alt:.2f}"
+    cv.putText(img, readout_text, (10,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+    
+
+    if recording:
+        video.write(img)
+
+
+    # resize to half size
+    img = cv.resize(img, (960,540))
+
+    # # draw a crosshair
     h,w = img.shape[:2]
     cv.line(img, (w//2-50,h//2), (w//2+50,h//2), (0,0,255), 1)
     cv.line(img, (w//2,h//2-50), (w//2,h//2+50), (0,0,255), 1)
+    if recording:
+        cv.circle(img, (w-30,30),20,(0,0,255),-1)
 
-    # draw current position
-    azi, alt = t.read_position()
-    cv.rectangle(img, (10,20), (200,0), (0,0,0), -1)
-    cv.putText(img, f"azi: {azi:.2f} alt: {alt:.2f}", (10,10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
-
+    if tracking:
+        cv.rectangle(img, (w-50, h-50), (w-20, h-20), (255,0,0), -1)
 
     cv.imshow("Camera Image", img)
 
     # if press q, break
 
     if cv.waitKey(1) == ord('q'):
-        break 
+        t.stop()
+        return False
+    
+    return True
 
+
+# main loop
+while True:
+    try:
+        if not main():
+            break
+    except Exception as e:
+        t.stop()
+        print(e)
+        break
