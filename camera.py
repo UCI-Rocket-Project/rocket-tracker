3b4a1b038155ee2c27e313b1a81eaf8bb7eac6cc
Eric Pedley <ericpedley@gmail.com>
2024-03-17 22:44:50 -0700

    add camera slowness debugging code

--- camera.py
+++ camera.py
@@ -1,17 +1,27 @@
 import camera_zwo_asi
 import cv2 as cv
 import numpy as np
+from time import time
 
 class Camera:
     def __init__(self, gain=300, exposure='auto'):
         self.camera = camera_zwo_asi.Camera(0)
         self.camera.set_control('Gain', gain)
         self.camera.set_control('Exposure', exposure)
+        self.blank_img = self.camera.get_roi().get_image()
 
     def take_picture(self) -> cv.Mat:
-        img: np.ndarray = self.camera.capture().get_image()
-        img = cv.cvtColor(img, cv.COLOR_BAYER_RGGB2BGR)
+        cam_start = time()
+        self.camera.capture(self.blank_img)
+        print(f"Cap took {time()-cam_start}")
+        img = cv.cvtColor(self.blank_img.get_image(), cv.COLOR_BAYER_RGGB2BGR)
         return img
+
+    def fill_picture(self, img, rgb) -> None:
+        cam_start = time()
+        self.camera.capture(img)
+        print(f"Cap took {time()-cam_start}")
+        cv.cvtColor(img, cv.COLOR_BAYER_RGGB2BGR, img)
     
     def set_gain(self, new_gain: int):
         self.camera.set_control('Gain', new_gain)
